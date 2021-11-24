29af59ea3ec0c544ae8f83d70067f2ce462d6fd4
Ubuntu <azureuser@lil-bench000003.aswgeaqmboqurngwuyywl1fb4e.phxx.internal.cloudapp.net>
2021-11-24 08:04:49 +0000

    traefik-2.4

---
+++ 8-cores-http-traefik-2.4/run.sh
@@ -0,0 +1,21 @@
+
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=http   --variable downstreamScheme=http   --json traefik_connections-1.json
+
+
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=http   --variable downstreamScheme=http   --json traefik_connections-2.json
+
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=http   --variable downstreamScheme=http   --json traefik_connections-4.json
+
+
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=http   --variable downstreamScheme=http   --json traefik_connections-8.json
+
+
+
