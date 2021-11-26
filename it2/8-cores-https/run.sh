6f2e599e5f5130e9018fa98eea2e444436b5979b
Ubuntu <azureuser@lil-bench000003.4aunmxazd5iejayalwbnaf0e3d.phxx.internal.cloudapp.net>
2021-11-26 08:14:52 +0000

    https

---
+++ it2/8-cores-https/run.sh
@@ -0,0 +1,75 @@
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-nginx   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json nginx_connections-1.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.4   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.4_connections-1.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.5   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.5_connections-1.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net3.1   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net3.1_connections-1.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net6   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 1   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net6_connections-1.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-nginx   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json nginx_connections-2.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.4   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.4_connections-2.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.5   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.5_connections-2.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net3.1   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net3.1_connections-2.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net6   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 2   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net6_connections-2.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-nginx   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json nginx_connections-4.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.4   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.4_connections-4.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.5   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.5_connections-4.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net3.1   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net3.1_connections-4.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net6   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 4   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net6_connections-4.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-nginx   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json nginx_connections-8.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.4   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.4_connections-8.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.5   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.5_connections-8.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net3.1   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net3.1_connections-8.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net6   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 8   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net6_connections-8.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-nginx   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 16   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json nginx_connections-16.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.4   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 16   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.4_connections-16.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-traefik-2.5   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 16   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json traefik-2.5_connections-16.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net3.1   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 16   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net3.1_connections-16.json
+
+
+crank --config ./proxy.benchmarks.yml   --scenario proxy-yarp-net6   --profile lil-bench-traefik   --load.variables.duration 60   --load.variables.connections 16   --load.variables.verb POST   --load.variables.bodyFile https://raw.githubusercontent.com/liliang/traefik-benchmark/master/512B.txt   --variable path=\/?s=512   --variable serverScheme=https   --variable downstreamScheme=https   --json yarp-net6_connections-16.json
+
